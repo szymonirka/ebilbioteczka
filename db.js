fdf80be995a5d2208dc78582f92752691f0390c0
SzymonMazurek <mazurekszymon718@gmail.com>
2025-06-11 20:34:30 +0200

    poprawienie wygladu wyszukiwarki

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Dupa69Z_Trupa8!@#$%^',
+    password: '',
     database: 'ebiblioteczka'
 });
 
