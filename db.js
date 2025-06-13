434fe0f02c256fc6a54de5e0fa7740df89ac6237
SzymonMazurek <mazurekszymon718@gmail.com>
2025-06-13 20:21:37 +0200

    poprawienie wywietlana

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: '192.168.0.3',
+    host: 'localhost',
     user: 'root',
-    password: 'KanTask123!',
+    password: '',
     database: 'ebiblioteczka'
 });
 
